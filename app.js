c6fd189dfc7587e6c07a814fd62a6f9772076908
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-28 19:38:20 +0530

    feat: required db connection and made connection to db post server initialization

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const connectToDB = require("./src/config/db");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -15,4 +16,5 @@ app.use(express.json());
 app.use(cors());
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
+  connectToDB();
 });
