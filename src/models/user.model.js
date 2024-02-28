a80596cf5bb8f820025648960f6e64bbf2a07f75
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-28 19:47:16 +0530

    feat: model for user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,31 @@
 // # User model
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  aadhardCardNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  DOB: {
+    type: Date,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
