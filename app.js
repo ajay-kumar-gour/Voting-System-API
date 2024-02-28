8210508eb098ed57c77fe197413a5734622680ac
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-28 19:11:51 +0530

    feat : express server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // # Express app initialization
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+// console.log(PORT);
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
