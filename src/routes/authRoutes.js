794f96d77d82cd934b0634d7bd8ccbfa5e617467
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-29 12:58:00 +0530

    feat: add router handler for registers

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,1 +1,8 @@
 //  # Authentication routes
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.model");
+
+router.post("/register", registerController);
+
+module.exports = router;
