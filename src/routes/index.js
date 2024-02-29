87ace70eb52d61a28e95eff87f0287c5fcc5ad60
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-29 12:57:21 +0530

    feat: add router handler for authToutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,10 @@
 // # Route loader
+const express = require("express");
+
+const router = express.Router();
+
+const authRoutes = require("./authRoutes");
+
+router.use("/api/auth", authRoutes);
+
+module.exports = router;
