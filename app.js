95531794bec20b936b10b7ff3dc47750902986f1
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-29 12:58:26 +0530

    mounted the route handler

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const connectToDB = require("./src/config/db");
 require("dotenv").config();
-
+const routes = require("./src/routes/index");
 const PORT = process.env.PORT || 3000;
 
 // console.log(PORT);
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-
+app.use(routes);
 app.use(cors());
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
