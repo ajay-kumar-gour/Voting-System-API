42a6baa36bb23428f6db75ad86df342581a29df8
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-01 17:09:31 +0530

    feat: add user registration function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,13 @@ const mongoose = require("mongoose");
 
 const User = require("../models/user.model");
 
-const registerController = (req, res) => {};
+const registerController = async (req, res) => {
+  // const { name, aadhardCardNumber, age, DOB, password } = req.body;
+  const userData = req.body;
+  const newUser = new User(userData);
+  await newUser.save();
+  res.status(201).send({ message: "user registered" });
+};
 const loginController = (req, res) => {};
 const logoutController = (req, res) => {};
 
