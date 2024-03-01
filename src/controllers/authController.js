b49ea800177aca385651a166e494488220db4331
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-01 17:20:09 +0530

    feat:bundled the registration logic in TRY-CATCH to handle errors

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,11 +5,18 @@ const mongoose = require("mongoose");
 const User = require("../models/user.model");
 
 const registerController = async (req, res) => {
-  // const { name, aadhardCardNumber, age, DOB, password } = req.body;
-  const userData = req.body;
-  const newUser = new User(userData);
-  await newUser.save();
-  res.status(201).send({ message: "user registered" });
+  try {
+    // const { name, aadhardCardNumber, age, DOB, password } = req.body;
+    const userData = req.body;
+    const newUser = new User(userData);
+    await newUser.save();
+    res.status(201).send({ message: "user register" });
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
 const loginController = (req, res) => {};
 const logoutController = (req, res) => {};
