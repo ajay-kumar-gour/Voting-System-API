434e864bb5f76e54a9d2e5abf679aafdc0ca4816
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-01 17:22:18 +0530

    feat: add candidate model

--- src/models/candidate.model.js
+++ src/models/candidate.model.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
 const candidateSchema = new mongoose.Schema({
-
-
-
-
-    
+  name: {
+    type: String,
+    required: true,
+  },
+  party: {
+    type: String,
+    required: true,
+  },
+  manifesto: {
+    type: String,
+  },
 });
+
+module.exports = mongoose.model("Candidate", candidateSchema);
