fc442c42ec0b65b695fb5cc5a02eca55b0a87119
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-02 21:59:18 +0530

    feat: Enhance error handling middleware for improved logging and responses

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,1 +1,19 @@
 // # Error handling middleware
+
+// errorMiddleware.js
+
+const errorHandler = (err, req, res, next) => {
+  console.error(`Error: ${err.message}`);
+  // Log the stack trace for further debugging
+  console.error(err.stack);
+
+  // Check if the error is a known error type with a defined status code
+  if (err.status) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    // For unhandled errors, send a generic error response
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = errorHandler;
