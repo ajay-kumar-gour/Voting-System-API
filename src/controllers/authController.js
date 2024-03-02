f4f8cc1d049686e4b22fd598342cf44e805368b2
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-02 21:50:17 +0530

    fix: Update registerController function to send correct success message

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,12 +5,12 @@ const mongoose = require("mongoose");
 const User = require("../models/user.model");
 
 const registerController = async (req, res) => {
+const registerController = async (req, res) => {
   try {
-    // const { name, aadhardCardNumber, age, DOB, password } = req.body;
     const userData = req.body;
     const newUser = new User(userData);
     await newUser.save();
-    res.status(201).send({ message: "user register" });
+    res.status(201).send({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).send({
       message: "Internal Server Error",
@@ -18,6 +18,8 @@ const registerController = async (req, res) => {
     });
   }
 };
+
+
 const loginController = (req, res) => {};
 const logoutController = (req, res) => {};
 
