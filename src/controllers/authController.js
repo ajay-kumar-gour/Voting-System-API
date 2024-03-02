81695e68494b3e7683c501a39e0d3c97a61423fe
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-02 21:51:19 +0530

    feat: Add error check for req.body in registerController function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,9 +5,14 @@ const mongoose = require("mongoose");
 const User = require("../models/user.model");
 
 const registerController = async (req, res) => {
-const registerController = async (req, res) => {
   try {
     const userData = req.body;
+    if (!userData || Object.keys(userData).length === 0) {
+      return res
+        .status(400)
+        .send({ message: "Request body is missing or empty" });
+    }
+
     const newUser = new User(userData);
     await newUser.save();
     res.status(201).send({ message: "User registered successfully" });
