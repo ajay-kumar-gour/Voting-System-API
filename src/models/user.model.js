8d4d8d398945eedd5be98d39f3d84043d2c20c9c
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-03 15:20:28 +0530

    update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
   },
   gender:{
   type: String,
-  enum: ["male", "female", "others"],
+  enum: ["Male", "Female", "Others"],
   required: true
   },
   DOB: {
