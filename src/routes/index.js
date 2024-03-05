43adabc9b2d977cea471fd19d3a052b60b8a804f
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-05 21:24:23 +0530

    add: defined the main route handler for candidates

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,10 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./authRoutes");
+const candidateRoutes = require("./candidateRoutes");
 
 router.use("/api/auth", authRoutes);
-router.use("/api/auth", candidateRoutes);
-router.use("/api/auth", liveResultRoutes);
+router.use("/api", candidateRoutes);
+router.use("/api", liveResultRoutes);
 
 module.exports = router;
