a436ec0165568da0b9f94fed00f3d675e758fe8f
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-06 23:41:13 +0530

    update: user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,6 +42,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   const user = this;
+  console.log(this);
   if (!user.isModified("password")) {
     return next();
   }
