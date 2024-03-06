02c867f83858fe95e00e39998d6e51ab60ed60d5
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-06 23:43:42 +0530

    feat: email validator

---
+++ src/services/emailCheck.js
@@ -0,0 +1,7 @@
+const isValidEmail = (email) => {
+  // Regular expression to validate email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+module.exports = isValidEmail;
