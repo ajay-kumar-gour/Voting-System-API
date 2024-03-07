2cae10db8472226981e1943f6e0b81274d9b027c
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-07 23:38:42 +0530

    add: route for votes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,6 @@ const candidateRoutes = require("./candidateRoutes");
 
 router.use("/api/auth", authRoutes);
 router.use("/api", candidateRoutes);
-router.use("/api", liveResultRoutes);
+router.use("/api/votes", voteRoutes);
 
 module.exports = router;
