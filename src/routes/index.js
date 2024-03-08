5ca8e038f65cd388d6b5ac64ac50888294cce88f
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-08 22:36:22 +0530

    update index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,8 @@
 // # Route loader
 const express = require("express");
-
 const router = express.Router();
-
 const authRoutes = require("./authRoutes");
 const candidateRoutes = require("./candidateRoutes");
-
 router.use("/api/auth", authRoutes);
 router.use("/api", candidateRoutes);
 router.use("/api/votes", voteRoutes);
