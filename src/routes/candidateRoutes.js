32537d0c38ed03cbd33d83578d30f5b1564e4dd7
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-09 21:44:03 +0530

    add: add authToken dependency

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+
+const authenticateToken = require("../middlewares/authMiddleware");
 const {
   getAllCandidatesController,
   getCandidateByIdController,
