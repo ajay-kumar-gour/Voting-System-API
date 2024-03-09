b9cfec6968e4c3a5e9e73047994a9ceae976b4ae
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-09 22:33:25 +0530

    Check if a candidate with the same party name already exists

--- src/controllers/candidateController.js
+++ src/controllers/candidateController.js
@@ -7,7 +7,22 @@ const createCandidateController = async (req, res) => {
     }
     // Extract candidate data from the request body
     const { name, party, manifesto } = req.body;
+    // Check if required fields are missing
+    if (!name || !party) {
+      return res
+        .status(400)
+        .json({ message: "Name and party are required fields" });
+    }
 
+    // Check if a candidate with the same party name already exists
+    const existingCandidate = await Candidate.findOne({ party });
+
+    // If a candidate with the same party name already exists, send a 409 Conflict response
+    if (existingCandidate) {
+      return res.status(409).json({
+        message: "A candidate with the same party name already exists",
+      });
+    }
     // Create a new candidate object
     const newCandidate = new Candidate({
       name,
@@ -23,6 +38,7 @@ const createCandidateController = async (req, res) => {
       success: true,
       messages: "candidate created successfully",
       createdCandidateDetails: savedCandidate,
+      JWTDecodedData: req.decodedData,
     });
   } catch (error) {
     // If an error occurs during the database operation, send a 500 Internal Server Error response
