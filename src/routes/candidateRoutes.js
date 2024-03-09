12792c56eba0e12a95a27b5f19d27cbdc816334f
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-09 21:25:30 +0530

    add:routes to create an candidate

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllCandidatesController,
   getCandidateByIdController,
+  createCandidateController,
 } = require("../controllers/candidateController");
 
 router.get("/candidates", getAllCandidatesController);
 router.get("/candidates/:id", getCandidateByIdController);
+router.post("/candidates",createCandidateController);
 
 module.exports = router;
