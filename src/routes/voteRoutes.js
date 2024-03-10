c7b1f4e8810442708bb8f884071b396e4ba4d669
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-10 11:17:14 +0530

    update the routes to GET candidate ID

--- src/routes/voteRoutes.js
+++ src/routes/voteRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authenticateToken = require("../middlewares/authMiddleware");
 
 const { castVoteController } = require("../controllers/voteController");
-router.post("/cast-vote", castVoteController);
+router.post("/cast-vote/:id", castVoteController);
 
 module.exports = router;
