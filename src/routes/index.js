693605b4edfc098b2a94b4dfdc2a9dfada5885fb
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-10 10:55:53 +0530

    add main route for voteRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 const authRoutes = require("./authRoutes");
 const candidateRoutes = require("./candidateRoutes");
+
+const voteRoutes = require("./voteRoutes");
 router.use("/api/auth", authRoutes);
 router.use("/api", candidateRoutes);
-// router.use("/api/votes", voteRoutes);
+router.use("/api/votes", voteRoutes);
 
 module.exports = router;
