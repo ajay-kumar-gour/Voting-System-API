c098a0f11846ad93c2dd6cb81b4bde707d541c4d
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-10 12:23:08 +0530

    add routes to show live vote results

--- src/routes/voteRoutes.js
+++ src/routes/voteRoutes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middlewares/authMiddleware");
 
-const { castVoteController } = require("../controllers/voteController");
-router.post("/cast-vote/:id", authenticateToken,castVoteController);
+const {
+  castVoteController,
+  showLiveVoteCount,
+} = require("../controllers/voteController");
+router.post("/cast-vote/:id", authenticateToken, castVoteController);
+router.get("/live-vote-count", showLiveVoteCount);
 
 module.exports = router;
