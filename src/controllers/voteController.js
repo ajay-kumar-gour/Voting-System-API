21f284d766372f18554ef55a9c91480e4fd4c50e
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-10 10:56:57 +0530

    add controller to votes

---
+++ src/controllers/voteController.js
@@ -0,0 +1,7 @@
+const Candidate = require("../models/candidate.model");
+const User = require("../models/user.model");
+const mongoose = require("mongoose");
+
+const castVoteController = async (req, res) => {};
+
+module.exports = { castVoteController };
